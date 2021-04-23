4b8a666c1c26221f6be69182845fd7a8ca4e58a7
Graham Hall <ghall89@me.com>
2021-04-23 16:41:44 +0000

    built database models

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,20 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    categoryData: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [1]
+      }
+    }
   },
   {
     sequelize,
